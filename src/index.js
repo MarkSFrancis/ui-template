5715c39747ff349249e67fd4965036dc091f3e5d
MarkSFrancis <mrkfrancis8@gmail.com>
2020-04-19 15:10:15 +0100

    Replaced http-server with webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import 'bootstrap';
+import $ from 'jquery';
+import './css/index.scss';
+
+window.$ = window.jQuery = $;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 24261
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.s[ac]ss$/i,
+      use: [
+        // Creates `style` nodes from JS strings
+        'style-loader',
+        // Translates CSS into CommonJS
+        'css-loader',
+        // Compiles Sass to CSS
+        'sass-loader',
+      ],
+    }]
+  },
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  stats: 'minimal'
+}
